10a5d095c65b64f4a42f9a10e6c0dc4835a66cbe
LAPTOP-4LGA03E1\kaspe <kasp724s@edu.ucl.dk>
2019-08-19 10:08:09 +0200

    implement backend for calories

---
+++ CalorieAndCarbohydrateMeasurement/ApplicationLayer/CalculaionModelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+
+namespace ApplicationLayer
+{
+   public class CalculaionModelView
+    {
+        public CalculaionModelView()
+        {
+            Calculation calc = new Calculation();
+        }
+
+        private double Calculate(Calculation calc, double caloriesPerHundred, double gram)
+        {
+            return calc.caloriesInGivenGram(caloriesPerHundred, gram);
+        }
+    }
+}
---
+++ CalorieAndCarbohydrateMeasurement/Domain/Calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Domain
+{
+    public class Calculation
+    {
+        private double CalcCaloriePerGram(double calories)
+        {
+            double result = 0.0;
+            result = calories / 100.0;
+
+            return result;
+        }
+
+
+        public double caloriesInGivenGram(double caloriesPerHundred, double gram)
+        {
+            double result = 0.0;
+            result = CalcCaloriePerGram(caloriesPerHundred) * gram;
+
+            return result;
+        }
+    }
+}
